a839e9d00759af2b553186f091685d8871241e8e
hanjia2014 <han@Hans-MacBook-Pro.local>
2015-01-21 20:26:55 +1300

    Aplus accounting service

---
+++ js/project/emailService.js
@@ -0,0 +1,31 @@
+﻿myApp.factory(
+            "emailService",
+            function ($http, $q) {
+
+                // Return public API.
+                return ({
+                    send: function (emailInstance) {
+                        return $http({
+                            method: "post",
+                            url: "https://mandrillapp.com/api/1.0/messages/send.json",
+                            data: {
+                                'key': 'd9cfBtCtNDJqXkVM7Aw2ng',
+                                'message': {
+                                    'from_email': emailInstance.from,
+                                    'to': [
+                                        {
+                                            'email': 'aplus.sherry@gmail.com',
+                                            'name': emailInstance.name,
+                                            'type': 'to'
+                                        }
+                                    ],
+                                    'autotext': 'true',
+                                    'subject': emailInstance.name + ' - ' + emailInstance.phone,
+                                    'html': '<p>Email recieved from ' + emailInstance.name + '</p>' + emailInstance.message
+                                }
+                            }
+                        });
+                    },
+
+                });
+            });
